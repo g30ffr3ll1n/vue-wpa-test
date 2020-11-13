b529cd12c05170f7f8e18bf8925c6dd30fefde04
g30ffr3ll1n <geoffrellin@remotodojo.com>
2020-11-13 15:42:07 +0800

    commit before pwa installation

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-
+import store from "./store";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import VueFilterDateFormat from "vue-filter-date-format";
+import { ValidationProvider, extend, ValidationObserver } from "vee-validate";
+import { required } from "vee-validate/dist/rules";
+Vue.use(VueFilterDateFormat);
+Vue.use(BootstrapVue);
+extend("required", required);
+Vue.component("ValidationProvider", ValidationProvider);
+Vue.component("ValidationObserver", ValidationObserver);
 Vue.config.productionTip = false;
-
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,21 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
-Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
-
-const router = new VueRouter({
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import Search from "./views/Search.vue";
+Vue.use(Router);
+export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/search",
+      name: "search",
+      component: Search
+    }
+  ]
 });
-
-export default router;
